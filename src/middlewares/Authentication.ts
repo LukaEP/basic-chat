7dd867e0df89a7b179a868322b395f1bb23fc243
LukaEP <luka.paiva3@gmail.com>
2021-05-19 11:11:59 -0300

    Middleware verifying token

---
+++ src/middlewares/Authentication.ts
@@ -0,0 +1,21 @@
+import { Request, Response, NextFunction } from "express";
+import { verify } from "jsonwebtoken";
+
+class Authentication {
+    async auth(req: Request, res: Response, next: NextFunction) {
+        let token = req.headers["authorization"];
+
+        if (!token) {
+            res.status(401).send({ message: "Unauthorized" });
+        }
+
+        try {
+            verify(token, process.env.JWT_SECRET);
+            next();
+        } catch(error) {
+            res.status(401).send({ message: "Unauthorized" });
+        }
+    }
+}
+
+export { Authentication };
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,15 +1,17 @@
 import { Router } from "express";
 import { UserController } from "../controllers/UserController";
 import { ChatController } from "../controllers/ChatController";
+import { Authentication } from "../middlewares/Authentication";
 
 const router = Router();
 
 const userController = new UserController();
 const chatController = new ChatController();
+const authenticationMiddleware = new Authentication();
 
 router.post("/user/login", userController.login);
 router.post("/user/create/user", userController.createNewUser);
-router.post("/chat/list/chats", chatController.listChats);
-router.post("/chat/new", chatController.newChat);
+router.post("/chat/list/chats",  authenticationMiddleware.auth, chatController.listChats);
+router.post("/chat/new", authenticationMiddleware.auth, chatController.newChat);
 
 export { router };
